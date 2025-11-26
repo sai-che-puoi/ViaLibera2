5e3783ed4bdf7655d9ea53c488b71547fb0a4f23
Andrea Iacono <andrea.iacono.nl@gmail.com>
2025-11-26 07:52:09 +0100

    fix "restart" svg

--- questionario/js/QuizUI.js
+++ questionario/js/QuizUI.js
@@ -106,7 +106,7 @@ export class QuizUI {
         resetBtn.className = 'sorting-reset-btn';
         resetBtn.id = `sorting_reset_${index}`;
         resetBtn.innerHTML = `
-            <svg width="16" height="16" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                 <path d="M3 12a9 9 0 0 1 9-9 9.75 9.75 0 0 1 6.74 2.74L21 8"></path>
                 <path d="M21 3v5h-5"></path>
                 <path d="M21 12a9 9 0 0 1-9 9 9.75 9.75 0 0 1-6.74-2.74L3 16"></path>
