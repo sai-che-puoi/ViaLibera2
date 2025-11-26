832f9875e6ad56e25f36dc3f72e81e338a2d49dc
Andrea Iacono <andrea.iacono.nl@gmail.com>
2025-11-27 00:20:51 +0100

    update google script url

--- questionario/js/script_key.js
+++ questionario/js/script_key.js
@@ -1,5 +1,5 @@
 export const googleSheetScriptUrl = 'https://script.google.com/macros/s/AKfycbzgaV5QocDMi29Ks_I5ZF4ppa8XPteZTte5ZShkOMlDcR5RYnwusU2VfbyRhvXJzhBd/exec';
-export const googleDriveFolderScriptUrl = 'https://script.google.com/macros/s/AKfycbwsaD5mGMZG7iLTBBcNYqWInSsLO1vj2DSiWtiDQ2m9YzOXE0-ociSp3z-6KCXPNl-Jyg/exec';
+export const googleDriveFolderScriptUrl = 'https://script.google.com/macros/s/AKfycbxJ2pqhoHQHDWyVL9HKisSpatZ4eJ30awOEkm9t5kXnOTgkMY1l77yOpXVge8jAko0U0Q/exec';
 
 
 
