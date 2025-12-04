05784d5af503d272ffae25526fad97a6a387d26f
Adrianominora <adriano.minora@gmail.com>
2025-12-04 09:36:26 +0100

    updated sheet id and extension

--- transcription/config.py
+++ transcription/config.py
@@ -1,10 +1,10 @@
 
 # config.py
 # GOOGLE SHEET CONFIGURATIONS
-SPREADSHEET_ID = "1oawenZihJ31zxC12kRZ6YrZk1WOg8nr1j_8r-P1R_l8"
+SPREADSHEET_ID = "1kA2w9puir-od51z3KHZwNI9q6pnsw5_RoYbvgCR8o4Y"
 SHEET_NAME = "Sheet1"
 AUDIO_COLUMN_NAME = "Registrazione Audio Originale"
-AUDIO_EXTENSION = "1kA2w9puir-od51z3KHZwNI9q6pnsw5_RoYbvgCR8o4Y"
+AUDIO_EXTENSION = "webm"
 TRANSCRIPTION_COLUMN_NAME = "Trascrizione"
 ENHANCED_COLUMN_NAME = "Trascrizione rivista da LLM"
 
