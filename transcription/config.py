11fa46e8ebadc8dcb36f5711c4b3591b730846ba
Adrianominora <adriano.minora@gmail.com>
2026-01-13 10:58:12 +0100

    transcription config

--- transcription/config.py
+++ transcription/config.py
@@ -1,12 +1,12 @@
 
 # config.py
 # GOOGLE SHEET CONFIGURATIONS
-SPREADSHEET_ID = "1kA2w9puir-od51z3KHZwNI9q6pnsw5_RoYbvgCR8o4Y"
+SPREADSHEET_ID = "160ujCYA6qijwVD0wLaHkJ0swYGPICMwsb_0aYn7rpr8"
 SHEET_NAME = "Sheet1"
-AUDIO_COLUMN_NAME = "Registrazione Audio Originale"
+AUDIO_COLUMN_NAME = "Second Audio URL" #"Registrazione Audio Originale"
 AUDIO_EXTENSION = "webm"
-TRANSCRIPTION_COLUMN_NAME = "Trascrizione"
-ENHANCED_COLUMN_NAME = "Trascrizione rivista da LLM"
+TRANSCRIPTION_COLUMN_NAME = "Trascrizione Secondo Audio" #"Trascrizione"
+ENHANCED_COLUMN_NAME = "Trascrizione Secondo Audio Rivista da LLM" #"Trascrizione rivista da LLM"
 
 # AI SERVICES
 ASSEMBLYAI_API_URL = "https://api.assemblyai.com/v2"
